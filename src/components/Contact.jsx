b3c8c820f48ca16785bf7e9408a2d745b964dbb8
Houssam Haraf <houssamharaf2@gmail.com>
2023-11-06 16:12:54 +0100

    Computer update 3d

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,8 +10,8 @@ import { slideIn } from "../utils/motion";
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
-    name: "Houssam Haraf",
-    email: "houssamharaf2@gmail.com",
+    name: "",
+    email: "",
     message: "",
   });
 
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Houssam Haraf",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "houssamharaf0@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./vvs/vvs.gltf");
+  const computer = useGLTF("./ffs/ffs.gltf");
 
   return (
     <mesh>
